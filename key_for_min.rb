d78177ff998ae8ae5437c71d3665c1a405fb06b3
Nikki Apostolakis <nikki.apostolakis@gmail.com>
2018-05-05 20:41:28 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,8 +3,8 @@
 
 def key_for_min_value(name_hash)
 
-  min_value;
-  min_key;
+  min_value = 0
+  min_key = String.new
   name_hash.each do |key, value|
       if min_key.length==0
         min_value=value
